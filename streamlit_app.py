0fe52f85caf205d4ef06169cfaf0a1ce4f479b8b
pcgmail <112226192+pcgmail@users.noreply.github.com>
2022-12-14 20:23:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit
+import pandas
 streamlit.title('Test App')
 streamlit.header('Breakfast Menu')
 streamlit.text('🐔 Bacon & eggs')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
